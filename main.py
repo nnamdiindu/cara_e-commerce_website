e0cb9f629b8ccf60e0c2d42e6de5ff0e7a2740ba
Lucky <luckyodi310@gmail.com>
2025-05-30 01:30:06 +0100

    Modify main.py

--- main.py
+++ main.py
@@ -48,8 +48,8 @@ class User(UserMixin, db.Model):
     __tablename__ = "user"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    name: Mapped[str] = mapped_column(String(50), nullable=False)
-    email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    name: Mapped[str] = mapped_column(String(150), nullable=False)
+    email: Mapped[str] = mapped_column(String(150), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String(150), nullable=False)
     create_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
 
@@ -61,7 +61,7 @@ class StoreCollection(db.Model):
     __tablename__ = "store_collection"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    brand_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    brand_name: Mapped[str] = mapped_column(String(150), nullable=False)
     description: Mapped[str] = mapped_column(String(200), nullable=False)
     filename: Mapped[str] = mapped_column(String(100), nullable=False)
     amount: Mapped[float] = mapped_column(Float, nullable=False)
@@ -78,7 +78,7 @@ class Order(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(50), nullable=False)
     amount: Mapped[float] = mapped_column(Float, nullable=False)
-    reference: Mapped[str] = mapped_column(String(50), nullable=False)
+    reference: Mapped[str] = mapped_column(String(150), nullable=False)
     status: Mapped[str] = mapped_column(String(20), nullable=False)
     paid_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
 
