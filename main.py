f0a52e477f4b506c48cd25b19bb338857df36b48
Lucky <luckyodi310@gmail.com>
2025-05-30 01:24:00 +0100

    Modify main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ class User(UserMixin, db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
     email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(String(50), nullable=False)
+    password: Mapped[str] = mapped_column(String(150), nullable=False)
     create_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
 
     # Relationship to orders
